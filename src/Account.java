230985a2f4542d3f6cd9618fe20181bc8f059baf
jmbury <justyna.m.bury@gmail.com>
2022-03-10 21:11:41 +0100

    bank application

--- src/Account.java
+++ src/Account.java
@@ -1,5 +1,24 @@
 public class Account {
-    String client;
-    int bilans;
+    private String name;
+    private String surname;
+    private int ID;
+    private int balance;
+
+    public Account(String name, String surname, int ID, int balance) {
+        this.name = name;
+        this.surname = surname;
+        this.ID = ID;
+        this.balance = balance;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void withdrawal(int cash) {
+        this.balance = balance-cash;
+
+
+    }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
 public class Main {
     public static void main(String[] args){
 
+        Account user1 = new Account("Jan", "Kowalski", 1234, 777);
+        Account user2 = new Account("Grazyna", "Nowak", 9876, 4444);
+
+        System.out.println(user1.getBalance());
+        user1.withdrawal(78);
+        System.out.println(user1.getBalance());
+
+
 
     }
 }
